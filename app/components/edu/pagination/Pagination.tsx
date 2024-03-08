136dee529f6f9b05d1a6bfcac504246a6f3f3015
KimWooHyun <ktkwhms3@gmail.com>
2024-03-09 01:21:12 +0900

    Fix: Pagination 파싱되지 않은 params로 조회 에러 수정

--- app/components/edu/pagination/Pagination.tsx
+++ app/components/edu/pagination/Pagination.tsx
@@ -1,6 +1,9 @@
 'use client';
 
+import { COURSE_CONVERT_OBJECTS } from '@constants/course';
 import { PAGINATION } from '@constants/pagination';
+import convertObjectToQueryString from '@utils/convertObjectToQueryString';
+import convertSearchParamsToCourseObject from '@utils/convertSearchParamsToCourse';
 import { CoursesContext } from 'app/(main)/edu/form';
 import { PaginationContext } from 'app/(main)/providers';
 import axios from 'axios';
@@ -9,12 +12,11 @@ import React from 'react';
 import Arrow from './Arrow';
 import PageNum from './PageNum';
 
-const getPageCount = (total: number, current: number) => {
-  if (!current) return [];
-
+const getPageCount = (endPage: number, currentPage: number) => {
+  if (!currentPage) return [];
   const point = Math.floor(PAGINATION.SHOW / 2);
-  const start = Math.max(current - point, 1);
-  const end = Math.min(current + point, total);
+  const start = Math.max(currentPage - point, 1);
+  const end = Math.min(currentPage + point, endPage);
   const length = end - start;
 
   return new Array(length + 1).fill(0).map((_, i) => start + i);
@@ -33,20 +35,28 @@ export default function Pagination({
   const currentPage = courseLength
     ? Math.floor(offset / PAGINATION.PAGES_LIMIT) + 1
     : 0;
-
   const endPage = Math.ceil(total / PAGINATION.PAGES_LIMIT);
-  const pages = getPageCount(total, currentPage);
+  const pages = getPageCount(endPage, currentPage);
 
   async function handleOnClick(num: number) {
-    const newParams =
-      searchParams?.toString() +
-      `&offset=${num * PAGINATION.PAGES_LIMIT}` +
-      `&count=${PAGINATION.PAGES_LIMIT}`;
+    const query = new URLSearchParams(searchParams?.toString());
+    const paramsObject = Object.keys(COURSE_CONVERT_OBJECTS).reduce(
+      (acc, cur) => {
+        return { ...acc, [cur]: query.getAll(cur) };
+      },
+      {},
+    );
+    const courseObject = convertSearchParamsToCourseObject(paramsObject);
+    const queryString = convertObjectToQueryString({
+      ...courseObject,
+      offset: `${(num - 1) * PAGINATION.PAGES_LIMIT}`,
+      count: `${PAGINATION.PAGES_LIMIT}`,
+    });
     await axios
-      .get(`${process.env.NEXT_PUBLIC_URL}/api/getOrgCourseList?${newParams}`)
+      .get(`${process.env.NEXT_PUBLIC_URL}/api/getOrgCourseList?${queryString}`)
       .then((res) => res.data)
       .then((data) => {
-        setOffset(num * PAGINATION.PAGES_LIMIT);
+        setOffset((num - 1) * PAGINATION.PAGES_LIMIT);
         setCourses(data.courses);
         setCourseCount(data.course_count);
       });
